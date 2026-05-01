21fddafbd9ee5308174eca3246219feb8bc2e8ac
tieuchi.embedded <tieuchi.embedded@gmail.com>
2026-05-01 12:41:20 +0700

    add lane_state topic

--- Brain/messages/lane_state.h
+++ Brain/messages/lane_state.h
@@ -3,5 +3,6 @@
 
 typedef struct {
     MessageHeader h;
-    float heading_err_rad;  // angle deviation from lane tangent
+    float heading_err_rad;   // angle deviation from lane tangent
+    float lateral_offset_m;  // lateral offset from lane center (meters, +right)
 } LaneState;
